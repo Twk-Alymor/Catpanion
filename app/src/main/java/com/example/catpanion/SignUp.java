36284b65478a1dc41f91cd44e490ef51f9eccad0
Twk-Alymor <hersh.aviles@gmail.com>
2022-12-11 01:42:49 +0800

    Signup Activity UI

--- app/src/main/java/com/example/catpanion/SignUp.java
+++ app/src/main/java/com/example/catpanion/SignUp.java
@@ -6,16 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class SignUp extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sign_up);
     }
 
-    public void toSignup(View v){
-        Intent i = new Intent(MainActivity.this, SignUp.class);
+    public void toLogin(View v){
+        Intent i = new Intent(SignUp.this, MainActivity.class);
         startActivity(i);
     }
 }
