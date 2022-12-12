b1f5ff193f069b1998a9b39ea655b9a2da897ab7
Twk-Alymor <hersh.aviles@gmail.com>
2022-12-13 01:56:51 +0800

    prepared App Bar

---
+++ app/src/main/java/com/example/catpanion/Home.java
@@ -0,0 +1,16 @@
+package com.example.catpanion;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+    }
+}
